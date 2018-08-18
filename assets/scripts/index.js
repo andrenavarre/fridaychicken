86e4b555b9703d591e82a9c0a1871d29ee3b9308
Denis Moulin <moulin.denis@gmail.com>
2018-08-18 12:23:40 -0400

    no auto play

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,6 @@ window.onload = function(){
   var animation = bodymovin.loadAnimation({
     container: document.getElementById('bm'),
     renderer: 'svg',
-    loop: true,
     autoplay: true,
     path: 'data.json'
   })
